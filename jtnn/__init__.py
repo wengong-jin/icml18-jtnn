2b3820f5458bc827d2ec531de925eabecd808f0d
Wengong Jin <acmgokun@gmail.com>
2018-04-22 13:22:55 -0400

    Update __init__.py

--- jtnn/__init__.py
+++ jtnn/__init__.py
@@ -1,7 +1,6 @@
 from mol_tree import Vocab, MolTree
 from jtnn_vae import JTNNVAE
 from jtprop_vae import JTPropVAE
-from jtnn_lm import JTNNLM
 from mpn import MPN, mol2graph
 from nnutils import create_var
 from datautils import MoleculeDataset, PropDataset
