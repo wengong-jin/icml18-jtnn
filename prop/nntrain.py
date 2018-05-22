30eb08cda8eaa6cf1d16a19a7e1bdb27d6685162
Wengong Jin <wengong@rosetta5.csail.mit.edu>
2018-05-21 23:21:33 -0400

    prop updated

--- prop/nntrain.py
+++ prop/nntrain.py
@@ -17,13 +17,23 @@ parser.add_option("-m", "--save_dir", dest="save_path")
 parser.add_option("-b", "--batch", dest="batch_size", default=50)
 parser.add_option("-w", "--hidden", dest="hidden_size", default=300)
 parser.add_option("-d", "--depth", dest="depth", default=3)
+parser.add_option("-e", "--epoch", dest="epoch", default=20)
+parser.add_option("-a", "--attention", dest="attention", default=0)
+parser.add_option("-p", "--dropout", dest="dropout", default=0.2)
 opts,args = parser.parse_args()
    
 batch_size = int(opts.batch_size)
 depth = int(opts.depth)
 hidden_size = int(opts.hidden_size)
+num_epoch = int(opts.epoch)
+attention = int(opts.attention)
+dropout = float(opts.dropout)
 
-encoder = MPN(hidden_size, depth)
+if attention > 0:
+    encoder = AttMPN(hidden_size, depth, dropout=dropout)
+else:
+    encoder = MPN(hidden_size, depth)
+
 model = nn.Sequential(
         encoder,
         nn.Linear(hidden_size, hidden_size), 
@@ -60,6 +70,7 @@ random.shuffle(train)
 
 def valid_loss(data):
     mse = 0
+    model.train(False)
     for i in xrange(0, len(data), batch_size):
         batch = data[i:i+batch_size]
         mol_batch, label_batch = zip(*batch)
@@ -69,11 +80,14 @@ def valid_loss(data):
         preds = model(mol_batch).view(-1)
         loss = loss_fn(preds, labels)
         mse += loss.data[0] * batch_size 
+
+    model.train(True)
     return mse / len(data)
      
-
-for epoch in xrange(20):
+best_loss = 1e5
+for epoch in xrange(num_epoch):
     mse,it = 0,0
+    print "learning rate: %.6f" % scheduler.get_lr()[0]
     for i in xrange(0, len(train), batch_size):
         batch = train[i:i+batch_size]
         mol_batch, label_batch = zip(*batch)
@@ -88,7 +102,7 @@ for epoch in xrange(20):
         loss.backward()
         optimizer.step()
 
-        if i % 100 == 0:
+        if i % 1000 == 0:
             pnorm = math.sqrt(sum([p.norm().data[0] ** 2 for p in model.parameters()]))
             gnorm = math.sqrt(sum([p.grad.norm().data[0] ** 2 for p in model.parameters()]))
             print "RMSE=%.4f,PNorm=%.2f,GNorm=%.2f" % (math.sqrt(mse / it), pnorm, gnorm)
@@ -96,9 +110,11 @@ for epoch in xrange(20):
             mse,it = 0,0
 
     scheduler.step()
-    print "learning rate: %.6f" % scheduler.get_lr()[0]
-    print "validation loss: %.4f" % valid_loss(valid)
+    cur_loss = valid_loss(valid)
+    print "validation loss: %.4f" % cur_loss
     if opts.save_path is not None:
         torch.save(model.state_dict(), opts.save_path + "/model.iter-" + str(epoch))
+        if cur_loss < best_loss:
+            best_loss = cur_loss
+            torch.save(model.state_dict(), opts.save_path + "/model.best")
 
-print "test loss: %.4f" % valid_loss(test)
