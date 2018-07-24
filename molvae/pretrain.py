0e8ab6a3ef47fa24398ec842b750d2884094c326
Wengong Jin <wengong@rosetta5.csail.mit.edu>
2018-07-23 21:17:23 -0400

    pretrain shuffle

--- molvae/pretrain.py
+++ molvae/pretrain.py
@@ -54,7 +54,7 @@ MAX_EPOCH = 3
 PRINT_ITER = 20
 
 for epoch in xrange(MAX_EPOCH):
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=lambda x:x, drop_last=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=lambda x:x, drop_last=True)
 
     word_acc,topo_acc,assm_acc,steo_acc = 0,0,0,0
 
