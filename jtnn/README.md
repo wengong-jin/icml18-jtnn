956288fb108d84a406f13124e6e4d6bc4ecaa635
Wengong Jin <wengong@rosetta5.csail.mit.edu>
2019-01-03 21:46:19 -0500

    initial push

--- jtnn/README.md
+++
@@ -1,7 +0,0 @@
-# Models and Tree Decomposition
-
-To perform tree decomposition over a set of molecules, run
-```
-python mol_tree.py < ../data/all.txt
-```
-This gives you the vocabulary of all cluster labels over the entire dataset.
--- molvae/sample.py
+++ molvae/sample.py
@@ -1,45 +1,33 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.optim.lr_scheduler as lr_scheduler
-from torch.autograd import Variable
 
 import math, random, sys
-from optparse import OptionParser
-from collections import deque
+import argparse
+from fast_jtnn import *
 import rdkit
-import rdkit.Chem as Chem
-from rdkit.Chem import Draw
-
-from jtnn import *
 
 lg = rdkit.RDLogger.logger() 
 lg.setLevel(rdkit.RDLogger.CRITICAL)
 
-parser = OptionParser()
-parser.add_option("-n", "--nsample", dest="nsample")
-parser.add_option("-v", "--vocab", dest="vocab_path")
-parser.add_option("-m", "--model", dest="model_path")
-parser.add_option("-w", "--hidden", dest="hidden_size", default=200)
-parser.add_option("-l", "--latent", dest="latent_size", default=56)
-parser.add_option("-d", "--depth", dest="depth", default=3)
-opts,args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--nsample', type=int, required=True)
+parser.add_argument('--vocab', required=True)
+parser.add_argument('--model', required=True)
+
+parser.add_argument('--hidden_size', type=int, default=450)
+parser.add_argument('--latent_size', type=int, default=56)
+parser.add_argument('--depthT', type=int, default=20)
+parser.add_argument('--depthG', type=int, default=3)
+
+args = parser.parse_args()
    
-vocab = [x.strip("\r\n ") for x in open(opts.vocab_path)] 
+vocab = [x.strip("\r\n ") for x in open(args.vocab)] 
 vocab = Vocab(vocab)
 
-hidden_size = int(opts.hidden_size)
-latent_size = int(opts.latent_size)
-depth = int(opts.depth)
-nsample = int(opts.nsample)
-
-model = JTNNVAE(vocab, hidden_size, latent_size, depth)
-load_dict = torch.load(opts.model_path)
-missing = {k: v for k, v in model.state_dict().items() if k not in load_dict}
-load_dict.update(missing) 
-model.load_state_dict(load_dict)
+model = JTNNVAE(vocab, args.hidden_size, args.latent_size, args.depthT, args.depthG)
+model.load_state_dict(torch.load(args.model))
 model = model.cuda()
 
 torch.manual_seed(0)
-for i in xrange(nsample):
-    print model.sample_prior(prob_decode=True)
+for i in xrange(args.nsample):
+    print model.sample_prior()
